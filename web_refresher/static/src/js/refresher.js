0d5d78e3866f8778deb982933c2236bf0c4154ee
Augusto Costa <gustotc@gmail.com>
2026-02-12 16:56:53 -0300

    Ajusta Botão de Refresh módulo 'web_refresher'
    - Adiciona para todas as outras visões
    - Posiciona antes dos botões de visão para ficar padronizado
    - Atualiza CSS

--- web_refresher/static/src/js/refresher.js
+++ web_refresher/static/src/js/refresher.js
@@ -3,23 +3,69 @@
 odoo.define('refresher.pager', function(require) {
     'use strict';
 
+    var AbstractController = require('web.AbstractController');
     var pager = require('web.Pager');
+    var core = require('web.core');
+
+    var _t = core._t;
+
+    function _buildRefreshButton() {
+        return $('<button>', {
+            type: 'button',
+            class: 'btn btn-secondary o_cp_refresh_button o_cp_refresh_button--accent',
+            title: _t('Refresh'),
+            'aria-label': _t('Refresh')
+        }).append($('<i>', {class: 'fa fa-refresh'}));
+    }
     pager.include({
         start: function() {
-            var self = this;
-            var res = self._super();
-
-            var $button = $('<span>', {
-                class: "fa fa-refresh btn btn-icon o_pager_refresh",
-                css: {"margin-right": "8px"},
-                "aria-label": "Refresh"
-            });
-            $button.on('click', function(){
-                self._changeSelection(0);
-            });
-
-            self.$el.prepend($button);
-            return res;
+            return this._super();
         }
+    });
+
+    AbstractController.include({
+        _renderControlPanelElements: function () {
+            var elements = this._super.apply(this, arguments);
+            if (!this.withControlPanel) {
+                return elements;
+            }
+
+            var $refresh = _buildRefreshButton();
+            $refresh.on('click', this._onRefreshClick.bind(this));
+
+            var $switchButtons = elements.$switch_buttons || $();
+            if ($switchButtons.find('.o_cp_refresh_button').length) {
+                return elements;
+            }
+
+            if ($switchButtons.length) {
+                elements.$switch_buttons = $refresh.add($switchButtons);
+            } else {
+                elements.$switch_buttons = $refresh;
+            }
+            return elements;
+        },
+
+        _onRefreshClick: function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            if (this._refreshInProgress) {
+                return;
+            }
+            this._refreshInProgress = true;
+            var $button = $(event.currentTarget);
+            $button.prop('disabled', true).addClass('o_cp_refresh_button--loading');
+            $button.find('.fa-refresh').addClass('fa-spin');
+
+            var self = this;
+            $.when(this.reload()).always(function () {
+                self._refreshInProgress = false;
+                if (self.isDestroyed()) {
+                    return;
+                }
+                $button.prop('disabled', false).removeClass('o_cp_refresh_button--loading');
+                $button.find('.fa-refresh').removeClass('fa-spin');
+            });
+        },
     });
 });
